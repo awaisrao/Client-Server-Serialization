ff397d269c7e41936fab480b308e9dd2a20b4ac3
awaisrao <awaisrao789@gmail.com>
2016-02-26 12:58:42 +0500

    Create DataClass.java

---
+++ DataClass.java
@@ -0,0 +1,17 @@
+package clientsocket;
+import java.net.*;
+import java.io.*;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author zulfiqar.bscs13seecs
+ */
+public class DataClass implements Serializable{
+    public String name;
+    public String notes;
+}
